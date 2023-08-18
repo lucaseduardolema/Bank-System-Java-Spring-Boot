87c5a9bf4f5e5318d3e6957279a9125236446f98
Lucas <lucaseduardolema@gmail.com>
2023-08-17 23:18:12 -0300

    inital commit

---
+++ src/main/java/com/banksystem/domain/user/User.java
@@ -0,0 +1,19 @@
+package com.banksystem.domain.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity(name="users")
+@Table(name="users")
+@Getter
+@Setter
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
