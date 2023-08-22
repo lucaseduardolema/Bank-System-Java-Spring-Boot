6bd35c66b46dabc9b2a2d102a058aa02549a4baa
Lucas <lucaseduardolema@gmail.com>
2023-08-22 10:05:30 -0300

    chore: add UserController

--- src/main/java/com/banksystem/domain/user/User.java
+++ src/main/java/com/banksystem/domain/user/User.java
@@ -1,31 +1,40 @@
 package com.banksystem.domain.user;
 
+import com.banksystem.dtos.UserDTO;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.math.BigDecimal;
 
-@Entity(name="users")
-@Table(name="users")
+@Entity(name = "users")
+@Table(name = "users")
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 @EqualsAndHashCode(of = "id")
 public class User {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String firstName;
-    private String lastName;
-    @Column(unique = true)
-    private String document;
-    @Column(unique = true)
-    private String email;
-    private String password;
-    private BigDecimal balance;
-    @Enumerated(EnumType.STRING)
-    private UserType userType;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  private String firstName;
+  private String lastName;
+  @Column(unique = true)
+  private String document;
+  @Column(unique = true)
+  private String email;
+  private String password;
+  private BigDecimal balance;
+  @Enumerated(EnumType.STRING)
+  private UserType userType;
+
+  public User(UserDTO data) {
+    this.firstName = data.firstName();
+    this.lastName = data.lastName();
+    this.document = data.document();
+    this.email = data.email();
+    this.password = data.password();
+    this.balance = data.balance();
+    this.userType = data.userType();
+  }
 }
--- src/main/java/com/banksystem/services/UserService.java
+++ src/main/java/com/banksystem/services/UserService.java
@@ -2,11 +2,13 @@ package com.banksystem.services;
 
 import com.banksystem.domain.user.User;
 import com.banksystem.domain.user.UserType;
+import com.banksystem.dtos.UserDTO;
 import com.banksystem.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 @Service
 public class UserService {
@@ -27,7 +29,17 @@ public class UserService {
     return this.repository.findUserById(id).orElseThrow(() -> new Exception("Usuário não encontrado"));
   }
 
+  public User createUser(UserDTO data) {
+    User newUser = new User(data);
+    this.saveUser(newUser);
+    return newUser;
+  }
+
   public void saveUser(User user) {
     this.repository.save(user);
+  }
+
+  public List<User> getAllUsers() {
+    return this.repository.findAll();
   }
 }
